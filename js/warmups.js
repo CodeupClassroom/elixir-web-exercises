774dfc5cee8d46e1f6bc37ecf5d65bfb688d0fe4
vegetasrevenge <cjedwards233@yahoo.com>
2022-01-14 09:49:16 -0600

    adds 1/14 warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,12 +1,46 @@
 "use strict;"
 
-function arrayToString(myArray) {
-    var myString = "";
-    for (let i = 0; i < myArray.length; i++) {
-        myString += myArray[i];
+// function arrayToString(myArray) {
+//     var myString = "";
+//     for (let i = 0; i < myArray.length; i++) {
+//         myString += myArray[i];
+//
+//     }
+//     return myString
+// }
+//
+// console.log(arrayToString(["hello", "world"]))
 
+function removeAll(myArray, valueToRemove) {
+
+    let filteredArr = [];
+
+    // negative logic approach - create new filtered array
+    // for (let i = 0; i < myArray.length; i++) {
+    //     if (myArray[i] !== valueToRemove){
+    //         filteredArr.push(myArray[i]);
+    //     }
+    // }
+
+    // affirmative logic approach - alter array in-place
+    // for (let i = 0; i < myArray.length; i++) {
+    //     if(myArray[i] === valueToRemove){
+    //         myArray.splice(i, 1);
+    //         i--
+    //     }
+    // }
+
+    // affirmative logic WITH filtered array
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] === valueToRemove) {
+            continue;
+        }
+        filteredArr.push(myArray[i]);
     }
-    return myString
+
+    return filteredArr; // or myArray
 }
 
-console.log(arrayToString(["hello", "world"]))
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+console.log(removeAll(bugs, "ant"));
