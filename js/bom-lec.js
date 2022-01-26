1b1e80df9bb3df3ae5b2f31473f4194da98bff5a
Ry Sutton <ryancesutton@gmail.com>
2022-01-26 11:58:37 -0600

    adding bom and dom lectures

---
+++ js/bom-lec.js
@@ -0,0 +1,31 @@
+"use strict";
+
+console.log("Hello from bom-lec1")
+
+// let numberOfTimesRun = 0;
+// let intervalFunction = function () {
+//     console.log("Logging every second");
+//     numberOfTimesRun++;
+//     if (numberOfTimesRun >= 10) {
+//         clearInterval(interval);
+//     }
+// }
+// let interval = setInterval(intervalFunction, 1000);
+//
+//
+// let timeOutFunction = function () {
+//     console.log("Logging after 5 seconds!")
+// }
+//
+// setTimeout(timeOutFunction, 5000);
+
+
+
+
+
+
+
+
+
+
+
