f7565d01bd6c490cb2e4374c848114f6ab7ec3bf
Ry Sutton <ryancesutton@gmail.com>
2022-02-04 10:09:56 -0600

    jquery mouse events lesson examples

--- js/jquery-events-lec.js
+++ js/jquery-events-lec.js
@@ -1,6 +1,36 @@
+// Using vanilla JS
+// document.getElementsByClassName('button').addEventListener('click', function () {
+//     console.log("A button was clicked");
+// })
+
 
 $(document).ready(function () {
 
+    alert('Hello from jquery');
 
+    $('li').click(function () {
+        $(this).css('background-color', 'yellow')
+    })
+
+    let paragraphHandler = function () {
+        console.log($(this).css('background-color'));
+        if ($(this).css('background-color') !== 'rgba(0, 0, 0, 0)'){
+            $(this).css('background-color', 'yellow')
+        } else {
+            $(this).css('background-color', 'red')
+        }
+    }
+
+    $('p').click(paragraphHandler)
+
+    let headerHandler = function () {
+        $(this).css('color', 'teal')
+    }
+    $('h1').dblclick(headerHandler)
+
+    $('.sec-intro').hover(
+        function () {
+            $(this).css('font-style', 'italic')
+        })
 
 })
